ad820628e263c34475c7f2329aca0858c650782f
Sussan Chamling <sussan.chamling@hazesoft.co>
2025-05-31 10:40:10 +0545

    docker environment setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response([
+        'status' => true
+    ]);
 });
